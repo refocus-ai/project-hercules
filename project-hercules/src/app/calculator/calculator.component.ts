320792597641337999b05cd90f11066c4df3fe10
Lauren Smalley <laurensmalley15@gmail.com>
2020-12-28 10:47:33 -0800

    fixed file strucure and added proper components and routing

--- project-hercules/src/app/calculator/calculator.component.ts
+++ project-hercules/src/app/calculator/calculator.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
 
+  showCalculatorForm =  true;
+
   constructor() { }
 
   ngOnInit(): void {
